2b11fdb708edacb3b254236525ea7e7886ee89de
Tanguy <tanguy.pruvot@gmail.com>
2010-06-16 12:46:09 +0200

    also, qi.nb0 not used to boot from sdcard... using empty file

--- initramfs/compress.sh
+++ initramfs/compress.sh
@@ -8,7 +8,7 @@ SDPART=/media/2B3F-7268
 #kernel to use
 ZIMAGE=./zImageMer
 ##ZIMAGE=./zImage2631q
-#ZIMAGE=./zImage34
+##ZIMAGE=./zImage34
 
 rm initrd.igz
 
@@ -17,8 +17,8 @@ rm initrd.igz
 mkimage -d tmp.gz -A ARM -O Linux -C gzip -T RAMDisk -n "SmartQ initramfs" initrd.igz
 
 #../fw-utils/mkSmartQ5 qi.nb0 u-boot.bin $ZIMAGE initrd.igz
-
-../fw-utils/mkSmartQ5 qi.nb0 no-u-boot.bin $ZIMAGE initrd.igz
+#../fw-utils/mkSmartQ5 qi.nb0 no-u-boot.bin $ZIMAGE initrd.igz
+../fw-utils/mkSmartQ5 no-qi.nb0 no-u-boot.bin $ZIMAGE initrd.igz
 
 rm $SDPART/SmartQ5
 cp SmartQ5 $SDPART/
