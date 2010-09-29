cb5d18a792691443c13aabfa2234bedeed716a21
Tanguy <tanguy.pruvot@gmail.com>
2010-09-30 01:11:15 +0200

    import mcuelenaere's qi-smartq

---
+++ qi-smartq/config.mk
@@ -0,0 +1,23 @@
+#
+# Include the make variables (CC, etc...)
+#
+
+CPU=s3c6410
+
+CROSS_PATH=/usr/local/software/toolchain-arm-jaunty.rgs1
+CROSS_COMPILE=${CROSS_PATH}/bin/arm-none-linux-gnueabi-
+
+COMPILER_LIB_PATH_PRE=${CROSS_PATH}/lib/gcc/arm-none-linux-gnueabi
+COMPILER_LIB_PATH=${COMPILER_LIB_PATH_PRE}/`ls ${COMPILER_LIB_PATH_PRE}`
+
+AS	= $(CROSS_COMPILE)as
+LD	= $(CROSS_COMPILE)ld
+CC	= $(CROSS_COMPILE)gcc
+OBJCOPY = $(CROSS_COMPILE)objcopy
+OBJDUMP = $(CROSS_COMPILE)objdump
+HOSTCC	= gcc
+
+# we need the mkudfu tool from U-Boot build
+#MKUDFU = ../uboot/u-boot/tools/mkudfu
+
+export CROSS_COMPILE AD LD CC OBJCOPY OBJDUMP MKUDFU
---
+++ qi-smartq/src/utils.c
@@ -0,0 +1,467 @@
+/*
+ * (C) Copyright 2008 Openmoko, Inc.
+ * Author: Andy Green <andy@openmoko.org>
+ *
+ * Little utils for print and strings
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <qi.h>
+#include <stddef.h>
+
+void (*putc_func)(char) = NULL;
+
+
+void set_putc_func(void (*p)(char))
+{
+	putc_func = p;
+}
+
+size_t strlen(const char *s)
+{
+	size_t n = 0;
+
+	while (*s++)
+		n++;
+
+	return n;
+}
+
+char *strcpy(char *dest, const char *src)
+{
+	char * dest_orig = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	*dest = '\0';
+
+	return dest_orig;
+}
+
+int puts(const char *string)
+{
+	while (*string)
+		(putc_func)(*string++);
+
+	return 1;
+}
+
+/* done like this to avoid needing statics in steppingstone */
+void printnybble(unsigned char n)
+{
+	if (n < 10)
+		(putc_func)('0' + n);
+	else
+		(putc_func)('a' + n - 10);
+}
+
+void print8(unsigned char n)
+{
+	printnybble((n >> 4) & 15);
+	printnybble(n & 15);
+}
+
+void print32(unsigned int u)
+{
+	print8(u >> 24);
+	print8(u >> 16);
+	print8(u >> 8);
+	print8(u);
+}
+
+void printdec(int n)
+{
+	int d[] = {
+		1 * 1000 * 1000 * 1000,
+		     100 * 1000 * 1000,
+		      10 * 1000 * 1000,
+		       1 * 1000 * 1000,
+			    100 * 1000,
+			     10 * 1000,
+			      1 * 1000,
+				   100,
+				    10,
+				     1,
+				     0
+	};
+	int flag = 0;
+	int div = 0;
+
+	if (n < 0) {
+		(putc_func)('-');
+		n = -n;
+	}
+
+	while (d[div]) {
+		int r = 0;
+		while (n >= d[div]) {
+			r++;
+			n -= d[div];
+		}
+		if (r || flag || (d[div] == 1)) {
+			(putc_func)('0' + r);
+			flag = 1;
+		}
+		div++;
+	}
+}
+
+void *memcpy(void *dest, const void *src, size_t n)
+{
+	u8 const * ps = src;
+	u8 * pd = dest;
+
+	while (n--)
+		*pd++ = *ps++;
+
+	return dest;
+}
+
+void *memset(void *s, int c, size_t n)
+{
+	u8 * p = s;
+
+	while (n--)
+		*p++ = c;
+
+	return s;
+}
+
+void delay(unsigned long n)
+{
+	while(n--)
+		udelay(1000000);
+}
+
+void udelay(unsigned long d)
+{
+	static int q;
+	int i;
+	unsigned long n;
+
+	if (d > 1000000) {
+		delay(d / 1000000);
+		d %= 1000000;
+	}
+	for (i = 0; i < 30; i++)
+		for (n = d; n; n--)
+			q += n * q;
+}
+
+size_t strnlen(const char *s, int max_size)
+{
+	size_t n = 0;
+
+	while (*s++ && n < max_size)
+		n++;
+
+	return n;
+}
+
+/*
+ *  linux/lib/vsprintf.c
+ *
+ *  Copyright (C) 1991, 1992  Linus Torvalds
+ */
+
+/* vsprintf.c -- Lars Wirzenius & Linus Torvalds. */
+/*
+ * Wirzenius wrote this portably, Torvalds fucked it up :-)
+ */
+
+/* we use this so that we can do without the ctype library */
+#define is_digit(c)	((c) >= '0' && (c) <= '9')
+
+static int skip_atoi(const char **s)
+{
+	int i=0;
+
+	while (is_digit(**s))
+		i = i*10 + *((*s)++) - '0';
+	return i;
+}
+
+#define ZEROPAD	1		/* pad with zero */
+#define SIGN	2		/* unsigned/signed long */
+#define PLUS	4		/* show plus */
+#define SPACE	8		/* space if plus */
+#define LEFT	16		/* left justified */
+#define SPECIAL	32		/* 0x */
+#define LARGE	64		/* use 'ABCDEF' instead of 'abcdef' */
+
+#define do_div(n,base) ({ \
+	int __res; \
+	__res = ((unsigned long) n) % (unsigned) base; \
+	n = ((unsigned long) n) / (unsigned) base; \
+	__res; \
+})
+
+static char * number(char * str, long num, int base, int size, int precision ,int type)
+{
+	char c,sign,tmp[66];
+	const char *digits="0123456789abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	if (type & LARGE)
+		digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	if (type & LEFT)
+		type &= ~ZEROPAD;
+	if (base < 2 || base > 36)
+		return 0;
+	c = (type & ZEROPAD) ? '0' : ' ';
+	sign = 0;
+	if (type & SIGN) {
+		if (num < 0) {
+			sign = '-';
+			num = -num;
+			size--;
+		} else if (type & PLUS) {
+			sign = '+';
+			size--;
+		} else if (type & SPACE) {
+			sign = ' ';
+			size--;
+		}
+	}
+	if (type & SPECIAL) {
+		if (base == 16)
+			size -= 2;
+		else if (base == 8)
+			size--;
+	}
+	i = 0;
+	if (num == 0)
+		tmp[i++]='0';
+	else while (num != 0)
+		tmp[i++] = digits[do_div(num,base)];
+	if (i > precision)
+		precision = i;
+	size -= precision;
+	if (!(type&(ZEROPAD+LEFT)))
+		while(size-->0)
+			*str++ = ' ';
+	if (sign)
+		*str++ = sign;
+	if (type & SPECIAL) {
+		if (base==8)
+			*str++ = '0';
+		else if (base==16) {
+			*str++ = '0';
+			*str++ = digits[33];
+		}
+	}
+	if (!(type & LEFT))
+		while (size-- > 0)
+			*str++ = c;
+	while (i < precision--)
+		*str++ = '0';
+	while (i-- > 0)
+		*str++ = tmp[i];
+	while (size-- > 0)
+		*str++ = ' ';
+	return str;
+}
+
+int vsprintf(char *buf, const char *fmt, va_list args)
+{
+	int len;
+	unsigned long num;
+	int i, base;
+	char * str;
+	const char *s;
+
+	int flags;		/* flags to number() */
+
+	int field_width;	/* width of output field */
+	int precision;		/* min. # of digits for integers; max
+				   number of chars for from string */
+	int qualifier;		/* 'h', 'l', or 'q' for integer fields */
+
+	for (str=buf ; *fmt ; ++fmt) {
+		if (*fmt != '%') {
+			*str++ = *fmt;
+			continue;
+		}
+
+		/* process flags */
+		flags = 0;
+		repeat:
+			++fmt;		/* this also skips first '%' */
+			switch (*fmt) {
+				case '-': flags |= LEFT; goto repeat;
+				case '+': flags |= PLUS; goto repeat;
+				case ' ': flags |= SPACE; goto repeat;
+				case '#': flags |= SPECIAL; goto repeat;
+				case '0': flags |= ZEROPAD; goto repeat;
+				}
+
+		/* get field width */
+		field_width = -1;
+		if (is_digit(*fmt))
+			field_width = skip_atoi(&fmt);
+		else if (*fmt == '*') {
+			++fmt;
+			/* it's the next argument */
+			field_width = va_arg(args, int);
+			if (field_width < 0) {
+				field_width = -field_width;
+				flags |= LEFT;
+			}
+		}
+
+		/* get the precision */
+		precision = -1;
+		if (*fmt == '.') {
+			++fmt;
+			if (is_digit(*fmt))
+				precision = skip_atoi(&fmt);
+			else if (*fmt == '*') {
+				++fmt;
+				/* it's the next argument */
+				precision = va_arg(args, int);
+			}
+			if (precision < 0)
+				precision = 0;
+		}
+
+		/* get the conversion qualifier */
+		qualifier = -1;
+		if (*fmt == 'h' || *fmt == 'l' || *fmt == 'q') {
+			qualifier = *fmt;
+			++fmt;
+		}
+
+		/* default base */
+		base = 10;
+
+		switch (*fmt) {
+		case 'c':
+			if (!(flags & LEFT))
+				while (--field_width > 0)
+					*str++ = ' ';
+			*str++ = (unsigned char) va_arg(args, int);
+			while (--field_width > 0)
+				*str++ = ' ';
+			continue;
+
+		case 's':
+			s = va_arg(args, char *);
+			if (!s)
+				s = "<NULL>";
+
+			len = strnlen(s, precision);
+
+			if (!(flags & LEFT))
+				while (len < field_width--)
+					*str++ = ' ';
+			for (i = 0; i < len; ++i)
+				*str++ = *s++;
+			while (len < field_width--)
+				*str++ = ' ';
+			continue;
+
+		case 'p':
+			if (field_width == -1) {
+				field_width = 2*sizeof(void *);
+				flags |= ZEROPAD;
+			}
+			str = number(str,
+				(unsigned long) va_arg(args, void *), 16,
+				field_width, precision, flags);
+			continue;
+
+
+		case 'n':
+			if (qualifier == 'l') {
+				long * ip = va_arg(args, long *);
+				*ip = (str - buf);
+			} else {
+				int * ip = va_arg(args, int *);
+				*ip = (str - buf);
+			}
+			continue;
+
+		case '%':
+			*str++ = '%';
+			continue;
+
+		/* integer number formats - set up the flags and "break" */
+		case 'o':
+			base = 8;
+			break;
+
+		case 'X':
+			flags |= LARGE;
+		case 'x':
+			base = 16;
+			break;
+
+		case 'd':
+		case 'i':
+			flags |= SIGN;
+		case 'u':
+			break;
+
+		default:
+			*str++ = '%';
+			if (*fmt)
+				*str++ = *fmt;
+			else
+				--fmt;
+			continue;
+		}
+		if (qualifier == 'l')
+			num = va_arg(args, unsigned long);
+		else if (qualifier == 'h') {
+			num = (unsigned short) va_arg(args, int);
+			if (flags & SIGN)
+				num = (short) num;
+		} else if (flags & SIGN)
+			num = va_arg(args, int);
+		else
+			num = va_arg(args, unsigned int);
+		str = number(str, num, base, field_width, precision, flags);
+	}
+	*str = '\0';
+	return str-buf;
+}
+
+int sprintf(char * buf, const char *fmt, ...)
+{
+	va_list args;
+	int i;
+
+	va_start(args, fmt);
+	i=vsprintf(buf,fmt,args);
+	va_end(args);
+
+	return i;
+}
+
+int printf(const char *fmt, ...)
+{
+    char buf[256];
+	va_list args;
+	int i;
+
+	va_start(args, fmt);
+	i = vsprintf(buf, fmt, args);
+	va_end(args);
+	puts(buf);
+
+	return i;
+}
