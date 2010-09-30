4475378acf3932f487dae90451d07e58d6ddc10f
Tanguy <tanguy.pruvot@gmail.com>
2010-09-30 02:10:59 +0200

    my crosscompiler path

--- qi-smartq/config.mk
+++ qi-smartq/config.mk
@@ -4,8 +4,17 @@
 
 CPU=s3c6410
 
-CROSS_PATH=/usr/local/software/toolchain-arm-jaunty.rgs1
+#CROSS_PATH=/usr/local/software/toolchain-arm-jaunty.rgs1
+#CROSS_COMPILE=${CROSS_PATH}/bin/arm-none-linux-gnueabi-
+
+#CROSS_PATH=/usr/local/software/arm-2010q1-188-arm-none-eabi
+#CROSS_COMPILE=${CROSS_PATH}/bin/arm-none-eabi-
+
+CROSS_PATH=/usr/local/software/arm-2010q1-188-arm-gnueabi
 CROSS_COMPILE=${CROSS_PATH}/bin/arm-none-linux-gnueabi-
+
+#CROSS_PATH=/usr/local/software/toolchain-arm-karmic.rgs0
+#CROSS_COMPILE=${CROSS_PATH}/bin/arm-none-linux-gnueabi-
 
 COMPILER_LIB_PATH_PRE=${CROSS_PATH}/lib/gcc/arm-none-linux-gnueabi
 COMPILER_LIB_PATH=${COMPILER_LIB_PATH_PRE}/`ls ${COMPILER_LIB_PATH_PRE}`
